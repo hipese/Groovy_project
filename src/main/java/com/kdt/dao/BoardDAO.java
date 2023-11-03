1e35d297066ba6c8f76549a24a17c844467a725e
JawonHwang <jackie4386@naver.com>
2023-11-03 15:19:59 +0900

    GROOV-9 Boards modified

--- src/main/java/com/kdt/dao/BoardDAO.java
+++ src/main/java/com/kdt/dao/BoardDAO.java
@@ -1,5 +1,37 @@
 package com.kdt.dao;
 
-public class BoardDAO {
+import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kdt.dto.BoardDTO;
+
+@Repository
+public class BoardDAO {
+	
+	@Autowired
+	private SqlSession db;
+
+	public int insertBoard(BoardDTO dto) {
+		return db.insert("Boards.insert", dto);
+	}
+	
+	public List<BoardDTO>selectBoardAll(){ 
+		return db.selectList("Boards.selectAll");
+	}
+	
+	public BoardDTO selectBoardBySeq(int seq){ 
+		return db.selectOne("Boards.selectBySeq",seq);
+	}
+	
+	public int deleteBoard(Integer seq) {
+		return db.delete("Boards.delete",seq);
+	}
+	
+	public int updateBoard(BoardDTO dto) {
+		return db.update("Board.update",dto);
+	}
+	
 }
