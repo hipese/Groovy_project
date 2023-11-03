45e0b6bf729241fa2487f2a3c0377f176e7675c7
Khwiba <typoon141@gmail.com>
2023-11-03 17:11:07 +0900

    GROOV-60 sign_document data add

--- src/main/java/com/kdt/controllers/SignController.java
+++ src/main/java/com/kdt/controllers/SignController.java
@@ -1,35 +1,51 @@
 package com.kdt.controllers;
 
+import java.io.File;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.kdt.dto.Sign_documentDTO;
 import com.kdt.services.Sign_documentService;
 
 @RestController
-@RequestMapping("/api/messages")
+@RequestMapping("/api/signlist")
 public class SignController {
 
 	@Autowired
-	Sign_documentService sd_serService;
+	Sign_documentService signservice;
 
 	@PostMapping
-	public ResponseEntity<String> post(@RequestBody Sign_documentDTO signData) throws Exception {
+	public ResponseEntity<String> post(Sign_documentDTO dto, MultipartFile[] files) throws Exception {
 
-		String docType = signData.getDocument_type();
-		String writer = signData.getWriter();
-		String title = signData.getTitle();
-		// 그 외 다른 필요한 데이터 처리
+		String upload = "e:/uploads";
+		File uploadPath = new File(upload);
+		if (!uploadPath.exists()) {
+			uploadPath.mkdir();
+		}
 
-		// 데이터를 로깅하거나 다른 작업을 수행
-		System.out.println("문서종류: " + docType);
-		System.out.println("기안작성자: " + writer);
-		System.out.println("제목: " + title);
-
+		for (MultipartFile file : files) {
+			String oriName = file.getOriginalFilename();
+			String sysName = UUID.randomUUID() + "_" + oriName;
+			file.transferTo(new File(uploadPath + "/" + sysName));
+		}
+		
+		String writer = dto.getWriter();
+		String document_type = dto.getDocument_type();
+		String contents = dto.getContents();
+		String recipient = dto.getRecipient();
+		int accept = dto.getAccept();
+		String title = dto.getTitle();
+		
+		System.out.println(writer + document_type + title + contents + recipient);
+		
+		signservice.insert(dto);
 		return ResponseEntity.ok("완전 성공!"); // 클라이언트에게 http 응답코드 200번대 반환
 	}
 }
