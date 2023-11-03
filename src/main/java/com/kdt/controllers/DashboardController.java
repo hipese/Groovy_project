f4a91b90125ff5f0fbecec3eeee44c0d82d4a0c9
권석경 <106041079+Rubiver@users.noreply.github.com>
2023-11-03 16:57:51 +0900

    GROOV-45 spring / user checkin time insert function and select function added

--- src/main/java/com/kdt/controllers/DashboardController.java
+++ src/main/java/com/kdt/controllers/DashboardController.java
@@ -5,22 +5,30 @@ import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kdt.dto.AttendenceDTO;
+import com.kdt.services.AttendenceService;
+
 @RestController
 @RequestMapping("/api/dash")
 public class DashboardController {
 	
-	@PostMapping
-	public ResponseEntity<String> checkIn(@RequestPart("id")String id, @RequestPart("date") String date, @RequestPart("time")String time) throws Exception{
+	@Autowired
+	private AttendenceService AtdService;
+
+	
+	@PostMapping("/checkin")
+	public ResponseEntity<Integer> checkIn(@RequestPart("id")String id, @RequestPart("date") String date, @RequestPart("time")String time) throws Exception{
 		System.out.println(id+" / "+date+" / "+time);
-//		String decodedTime = URLDecoder.decode(checkInTime,"utf8");
-//		System.out.println(decodedTime);
 		         
          String combinedString = date + " " + time;
          
@@ -32,8 +40,47 @@ public class DashboardController {
          Timestamp timestamp = new Timestamp(formeddate.getTime());
          
          System.out.println("타임스탬프: " + timestamp);
+         
+         AttendenceDTO dto = new AttendenceDTO();
+         
+         dto.setId(id);
+         dto.setWorkstart(timestamp);
+         
+         int result = AtdService.insertCheckIn(dto);
 		
-		return ResponseEntity.ok().build();
+		return ResponseEntity.ok(result);
 	}
+	
+	@PostMapping("/checkout")
+	public ResponseEntity<Integer> checkout(@RequestPart("id")String id, @RequestPart("date") String date, @RequestPart("time")String time) throws Exception{
+		System.out.println(id+" / "+date+" / "+time);
+		         
+         String combinedString = date + " " + time;
+         
+         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy. MM. d. HH:mm:ss");
+         Date formeddate = dateFormat.parse(combinedString);
+         
+         
+         Timestamp timestamp = new Timestamp(formeddate.getTime());
+         
+         System.out.println("타임스탬프: " + timestamp);
+         
+         AttendenceDTO dto = new AttendenceDTO();
+         
+         dto.setId(id);
+         dto.setWorkend(timestamp);
+         
+		return ResponseEntity.ok(1);
+	}
+	
+	@GetMapping("/workstart/{id}")
+	public ResponseEntity<AttendenceDTO> workstart(@PathVariable String id){
+		System.out.println(id);
+		AttendenceDTO dto = AtdService.selectWorkStart(id);		
+		
+		return ResponseEntity.ok(dto);
+	}
+	
+	
 
 }
--- src/main/java/com/kdt/dao/AttendenceDAO.java
+++ src/main/java/com/kdt/dao/AttendenceDAO.java
@@ -4,12 +4,20 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.kdt.dto.AttendenceDTO;
+
 @Repository
 public class AttendenceDAO {
 	
 	@Autowired
 	private SqlSession db;
 	
+	public int insertCheckIn(AttendenceDTO dto) {
+		return db.insert("Attendence.insertCheckIn", dto);
+	}
 	
+	public AttendenceDTO selectWorkStart(String id) {
+		return db.selectOne("Attendence.selectWorkStart",id);
+	}
 
 }
