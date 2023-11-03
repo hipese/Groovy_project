bd369094e2c4aa6ee2995fdfb390751c8bbe85f3
권석경 <106041079+Rubiver@users.noreply.github.com>
2023-11-03 15:50:08 +0900

    GROOV-45 attendence dto, dao, service write, dashboardcontroller created

--- src/main/java/com/kdt/dao/AttendenceDAO.java
+++ src/main/java/com/kdt/dao/AttendenceDAO.java
@@ -1,5 +1,15 @@
 package com.kdt.dao;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class AttendenceDAO {
+	
+	@Autowired
+	private SqlSession db;
+	
+	
 
 }
--- src/main/java/com/kdt/dto/AttendenceDTO.java
+++ src/main/java/com/kdt/dto/AttendenceDTO.java
@@ -1,5 +1,56 @@
 package com.kdt.dto;
 
+import java.sql.Timestamp;
+
 public class AttendenceDTO {
 
+	private int attendanceid;
+	private String id;
+	private Timestamp workstart;
+	private Timestamp workend;
+	private Timestamp overendtime;
+	public AttendenceDTO() {
+		super();
+	}
+	public AttendenceDTO(int attendanceid, String id, Timestamp workstart, Timestamp workend, Timestamp overendtime) {
+		super();
+		this.attendanceid = attendanceid;
+		this.id = id;
+		this.workstart = workstart;
+		this.workend = workend;
+		this.overendtime = overendtime;
+	}
+	public int getAttendanceid() {
+		return attendanceid;
+	}
+	public void setAttendanceid(int attendanceid) {
+		this.attendanceid = attendanceid;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Timestamp getWorkstart() {
+		return workstart;
+	}
+	public void setWorkstart(Timestamp workstart) {
+		this.workstart = workstart;
+	}
+	public Timestamp getWorkend() {
+		return workend;
+	}
+	public void setWorkend(Timestamp workend) {
+		this.workend = workend;
+	}
+	public Timestamp getOverendtime() {
+		return overendtime;
+	}
+	public void setOverendtime(Timestamp overendtime) {
+		this.overendtime = overendtime;
+	}
+	
+	
+	
 }
