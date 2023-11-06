6e3971670bbe8f3d55f4626c087dcce5e96a0a59
KMS <msg_1218@naver.com>
2023-11-06 16:36:07 +0900

    GROOV-25 사내 주소록 로드 기능 추가

--- src/main/java/com/kdt/dao/ContactDAO.java
+++ src/main/java/com/kdt/dao/ContactDAO.java
@@ -1,12 +1,29 @@
 package com.kdt.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.kdt.dto.ContactDTO;
+import com.kdt.dto.MemberDTO;
 
 @Repository
 public class ContactDAO {
 
 	@Autowired
 	private SqlSession db;
+	
+	public List<ContactDTO> selectAll() {
+		return db.selectList("Contact.selectAll");
+	}
+	
+	public int insert(MemberDTO dto) {
+		return db.insert("Contact.insert", dto);
+	}
+	
+	public List<String> favorite(String id) {
+		return db.selectList("Contact.favorite", id);
+	}
 }
