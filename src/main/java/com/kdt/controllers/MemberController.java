b36b67ef90c732b9bfd2cc54033cdcf95063596e
hipes <ansj2916@gmail.com>
2023-11-07 13:37:55 +0900

    이미지 저장하기

--- src/main/java/com/kdt/controllers/MemberController.java
+++ src/main/java/com/kdt/controllers/MemberController.java
@@ -1,14 +1,20 @@
 package com.kdt.controllers;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.kdt.dto.MemberDTO;
 import com.kdt.services.MemberService;
@@ -18,30 +24,49 @@ import jakarta.servlet.http.HttpSession;
 @RestController
 @RequestMapping("/api/member")
 public class MemberController {
-	
+
 	@Autowired
 	MemberService mservice;
-	
+
 	@Autowired
 	HttpSession session;
-	
+
 	@GetMapping
-	public ResponseEntity<MemberDTO> memberProfile(){
-		String id = (String)session.getAttribute("loginID");
-		System.out.println("로그인 한놈은 누구인가?"+id);
-		
+	public ResponseEntity<MemberDTO> memberProfile() {
+		String id = (String) session.getAttribute("loginID");
+		System.out.println("로그인 한놈은 누구인가?" + id);
+
 		MemberDTO dto = mservice.getprofile(id);
-		System.out.println(dto.getContact()+" : "+dto.getGroup_name()+" : "+dto.getPosition());
+		System.out.println(dto.getContact() + " : " + dto.getGroup_name() + " : " + dto.getPosition());
 		return ResponseEntity.ok(dto);
 	}
-	
-	@GetMapping("/{fileName}")
-	public ResponseEntity<String> updateImage(@PathVariable String fileName){
-		String id = (String)session.getAttribute("loginID");
-		System.out.println("변경할 이미지 뭐로옴?:"+fileName);
+
+	@PostMapping
+	public ResponseEntity<String> updateImage(@RequestParam("cfile") MultipartFile cfile) throws Exception {
 		
-		mservice.updateImage(id,fileName);
-		return ResponseEntity.ok("저장 성공");
+		String id = (String) session.getAttribute("loginID");
+		System.out.println("변경할 이미지 뭐로옴?:" + cfile.getName());
+
+		mservice.updateImage(id, cfile.getName());
+		
+		
+		String fileName = StringUtils.cleanPath(cfile.getOriginalFilename());
+
+		Path storageLocation = Paths.get("C:\\ReactWorkSpace\\groovy\\src\\assets");
+		
+		try {
+			if (cfile.isEmpty()) {
+				throw new Exception("Failed to store empty file " + fileName);
+			}
+			// 파일의 경로를 생성합니다.
+			Path destinationFile = storageLocation.resolve(Paths.get(fileName)).normalize().toAbsolutePath();
+			// 파일을 저장합니다.
+			Files.copy(cfile.getInputStream(), destinationFile, StandardCopyOption.REPLACE_EXISTING);
+			return ResponseEntity.ok("File uploaded successfully: " + fileName);
+		} catch (IOException e) {
+			throw new Exception("Failed to store file " + fileName, e);
+		}
+		
 	}
 
 }
