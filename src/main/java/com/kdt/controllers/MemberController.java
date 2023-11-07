f0e33df731a6859182c338ba67685d91b51eeb68
hipes <ansj2916@gmail.com>
2023-11-07 12:15:25 +0900

    병합전 커밋

--- src/main/java/com/kdt/controllers/MemberController.java
+++ src/main/java/com/kdt/controllers/MemberController.java
@@ -3,6 +3,7 @@ package com.kdt.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,17 +30,17 @@ public class MemberController {
 		String id = (String)session.getAttribute("loginID");
 		System.out.println("로그인 한놈은 누구인가?"+id);
 		
-		MemberDTO dto =mservice.getprofile(id);
+		MemberDTO dto = mservice.getprofile(id);
 		System.out.println(dto.getContact()+" : "+dto.getGroup_name()+" : "+dto.getPosition());
 		return ResponseEntity.ok(dto);
 	}
 	
-	@PostMapping
-	public ResponseEntity<String> updateImage(@RequestBody String previewSrc){
+	@GetMapping("/{fileName}")
+	public ResponseEntity<String> updateImage(@PathVariable String fileName){
 		String id = (String)session.getAttribute("loginID");
-		System.out.println("변경할 이미지 뭐로옴?:"+previewSrc);
+		System.out.println("변경할 이미지 뭐로옴?:"+fileName);
 		
-		mservice.updateImage(id,previewSrc);
+		mservice.updateImage(id,fileName);
 		return ResponseEntity.ok("저장 성공");
 	}
 
