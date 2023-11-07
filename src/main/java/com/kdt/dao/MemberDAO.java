f3ccbd6ab28a57c915771d86bfbb41b707bb9616
hipes <ansj2916@gmail.com>
2023-11-07 11:20:10 +0900

    병합 오류 수정 및 맴버 플로필 코드

--- src/main/java/com/kdt/dao/MemberDAO.java
+++ src/main/java/com/kdt/dao/MemberDAO.java
@@ -1,5 +1,7 @@
 package com.kdt.dao;
 
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -14,5 +16,10 @@ public class MemberDAO {
 	
 	public MemberDTO getprofile(String id) {
 		return db.selectOne("Member.selectprofile",id);
+	}
+	
+	public int updateImage(String id, Map<String, Object> params) {
+		System.out.println(params);
+		return db.update("Member.ImageChange",params);
 	}
 }
