78b4a0eaea221e2305b79d2e447d90e30ae951a8
hipes <ansj2916@gmail.com>
2023-11-09 16:27:46 +0900

    DTO수정

--- src/main/java/com/kdt/dto/DepartmentDTO.java
+++ src/main/java/com/kdt/dto/DepartmentDTO.java
@@ -1,30 +1,26 @@
 package com.kdt.dto;
 
 public class DepartmentDTO {
-	private int dept_code;
-	private String group_name;
+	private String dept_name;
 	
 	public DepartmentDTO() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
-	public DepartmentDTO(int dept_code, String group_name) {
+
+	public DepartmentDTO(String dept_name) {
 		super();
-		this.dept_code = dept_code;
-		this.group_name = group_name;
+		this.dept_name = dept_name;
 	}
-	public int getDept_code() {
-		return dept_code;
+
+	public String getDept_name() {
+		return dept_name;
 	}
-	public void setDept_code(int dept_code) {
-		this.dept_code = dept_code;
+
+	public void setDept_name(String dept_name) {
+		this.dept_name = dept_name;
 	}
-	public String getGroup_name() {
-		return group_name;
-	}
-	public void setGroup_name(String group_name) {
-		this.group_name = group_name;
-	}
+	
+	
 	
 }
