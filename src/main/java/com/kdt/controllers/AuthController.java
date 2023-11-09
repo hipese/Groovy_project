0917cca0775b62a843ad781f5acae3708f0e18e4
hipes <ansj2916@gmail.com>
2023-11-09 20:10:15 +0900

    logout만들기

--- src/main/java/com/kdt/controllers/AuthController.java
+++ src/main/java/com/kdt/controllers/AuthController.java
@@ -21,13 +21,13 @@ public class AuthController {
 
 	@Autowired
 	private HttpSession session;
-	
+
 	@Autowired
 	private AuthService service;
-	
+
 	@PostMapping("login")
 	public ResponseEntity<String> login(@RequestBody LoginDTO loginDTO) throws Exception {
-		if(service.isMember(loginDTO)) {
+		if (service.isMember(loginDTO)) {
 			session.setAttribute("loginID", loginDTO.getId());
 			System.out.println("ID :" + loginDTO.getId());
 			return ResponseEntity.ok(loginDTO.getId());
@@ -35,15 +35,23 @@ public class AuthController {
 			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
 		}
 	}
-	
+
+	@PostMapping("logout")
+	public ResponseEntity<String> logout() {
+		String id = (String) session.getAttribute("loginID");
+		if (id != null && !id.isEmpty()) {
+//			session.removeAttribute("loginID"); 
+			return ResponseEntity.ok("로그아웃 되었습니다.");
+		}
+		return ResponseEntity.badRequest().body("로그인되어 있지 않습니다.");
+	}
+
 	@GetMapping("isLogined")
 	public ResponseEntity<String> isLogined() {
-		String id = (String)session.getAttribute("loginID");
+		String id = (String) session.getAttribute("loginID");
 		return ResponseEntity.ok(id);
 	}
-	
-	
-	
+
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<Void> ExceptionHandler(Exception e) {
 		e.printStackTrace();
