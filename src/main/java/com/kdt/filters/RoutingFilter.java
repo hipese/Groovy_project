e9e70cbbd532b0b3de7ff8de33a52aa1f8d799ee
Khwiba <typoon141@gmail.com>
2023-11-10 09:55:03 +0900

    GROOV-88 routing filter 추가

---
+++ src/main/java/com/kdt/filters/RoutingFilter.java
@@ -0,0 +1,45 @@
+package com.kdt.filters;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Component
+public class RoutingFilter implements Filter {
+ 
+    @Override
+    public void doFilter(ServletRequest request,
+                         ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        String requestURI = req.getRequestURI();
+        System.out.println(requestURI);
+        
+        if(requestURI.startsWith("/ws-message")) {
+            chain.doFilter(request, response);
+            return;
+        }
+        
+        if(requestURI.startsWith("/auth")) {
+            chain.doFilter(request, response);
+            return;
+        }
+        
+        if(requestURI.startsWith("/api")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        // all requests not api or static will be forwarded to index page. 
+        request.getRequestDispatcher("/").forward(request, response);
+    }
+
+}
