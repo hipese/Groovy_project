9a57544d00cbfe5a56b644fcff29a45bd4c2642e
hipes <ansj2916@gmail.com>
2023-11-22 10:28:22 +0900

    sysout제거

--- src/main/java/com/kdt/services/AdminService.java
+++ src/main/java/com/kdt/services/AdminService.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
 
 import com.kdt.dao.AdminDAO;
 import com.kdt.dao.VacationBalancesDAO;
